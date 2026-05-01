edb5213f02e4dcabd566b686e76cef1451f626e3
markryan3421 <c20223192.zipagan@csav.edu.ph>
2026-05-01 14:33:09 +0800

    Fix typo import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router"
 
+import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage"
 import NoteDetailPage from "./pages/NoteDetailPage"
-import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
